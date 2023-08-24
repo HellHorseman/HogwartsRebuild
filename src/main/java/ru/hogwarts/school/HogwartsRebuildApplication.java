6615cf27951568967288aaead36beaae24fce253
HellHorseman <duke.sheffer@yandex.ru>
2023-08-24 23:41:40 +0400

    add tests

--- src/main/java/ru/hogwarts/school/HogwartsRebuildApplication.java
+++ src/main/java/ru/hogwarts/school/HogwartsRebuildApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition
 public class HogwartsRebuildApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HogwartsRebuildApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsRebuildApplication.class, args);
+    }
 
 }
--- src/test/java/ru/hogwarts/school/Controller/FacultyControllerTemplateTest.java
+++ src/test/java/ru/hogwarts/school/Controller/FacultyControllerTemplateTest.java
@@ -1,6 +1,5 @@
 package ru.hogwarts.school.Controller;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
